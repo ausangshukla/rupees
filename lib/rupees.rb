b2c73b7ee37f1ba2652fa2217bc8442a539da83a
Abhay Chauhan <abhaychauhan@Abhays-MacBook-Air.local>
2024-06-07 10:46:31 +0530

    Handle one hundred crore

--- lib/rupees.rb
+++ lib/rupees.rb
@@ -20,13 +20,16 @@ module NumberToRupees
 
         # I would prefer One Rupee, insead of One Rupees
         return 'One Rupee.' if num == 1
-
-        if num > 99
-          num.to_s.rjust(11, '0').insert(-4, '0').scan(/../).each_with_index { |x, i| result += def_calc(x, i) }
+        if num < 1000000000
+          if num > 99
+            num.to_s.rjust(11, '0').insert(-4, '0').scan(/../).each_with_index { |x, i| result += def_calc(x, i) }
+          else
+            result = spell_two_digits(num)
+          end
+          result.sub(/,\s$/, '')
         else
-          result = spell_two_digits(num)
+          large_digit_convertor(num)
         end
-        result.sub(/,\s$/, '')
       end
 
       protected
@@ -49,6 +52,47 @@ module NumberToRupees
 
         r, f = x.divmod(10)
         f.zero? ? "#{WORDS[r*10]}" : "#{WORDS[r*10]} #{WORDS[f]}"
+      end
+
+      def large_digit_convertor(num)
+        crores = num / 10**7
+        remaining = num % 10**7
+        lakhs = remaining / 10**5
+        remaining = remaining % 10**5
+        thousands = remaining / 10**3
+        remaining = remaining % 10**3
+        hundreds = remaining / 10**2
+        remaining = remaining % 10**2
+
+        crore_text = crores > 0 ? "#{convert_to_words(crores)} Crore" : ""
+        lakh_text = lakhs > 0 ? "#{convert_to_words(lakhs)} Lakh" : ""
+        thousand_text = thousands > 0 ? "#{convert_to_words(thousands)} Thousand" : ""
+        hundred_text = hundreds > 0 ? "#{convert_to_words(hundreds)} Hundred" : ""
+        remaining_text = remaining > 0 ? "#{convert_to_words(remaining)}" : ""
+
+        words = [crore_text, lakh_text, thousand_text, hundred_text, remaining_text].reject(&:empty?).join(", ")
+        return (words + " Rupees")
+      end
+
+      def convert_to_words(num)
+        ones = %w[Zero One Two Three Four Five Six Seven Eight Nine]
+        teens = %w[Ten Eleven Twelve Thirteen Fourteen Fifteen Sixteen Seventeen Eighteen Nineteen]
+        tens = %w[Zero Ten Twenty Thirty Forty Fifty Sixty Seventy Eighty Ninety]
+      
+        if num < 10
+          ones[num]
+        elsif num < 20
+          teens[num - 10]
+        elsif num < 100
+          tens_place = num / 10
+          ones_place = num % 10
+          ones_place == 0 ? tens[tens_place] : "#{tens[tens_place]} #{ones[ones_place]}"
+        elsif num < 1000
+          hundreds_place = num / 100
+          remainder = num % 100
+          remainder_text = remainder > 0 ? " #{convert_to_words(remainder)}" : ""
+          "#{ones[hundreds_place]} Hundred#{remainder_text}"
+        end
       end
     end
   end
