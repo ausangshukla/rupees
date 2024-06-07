f927c83717cb0110db41296856bc0345d54456b8
Abhay Chauhan <abhaychauhan@Abhays-MacBook-Air.local>
2024-06-07 10:49:57 +0530

    Comment

--- lib/rupees.rb
+++ lib/rupees.rb
@@ -1,3 +1,4 @@
+# Handles upto 9999999999 - "Nine Hundred Ninety Nine Crore, Ninety Nine Lakh, Ninety Nine Thousand, Nine Hundred, Ninety Nine Rupees"
 module NumberToRupees
   WORDS = { 0 => 'zero', 1 => 'One', 2 => 'Two', 3 => 'Three', 4 => 'Four', 5 => 'Five', 6 => 'six', 7 => 'seven', 8 => 'Eight', 9 => 'Nine',
             10 => 'Ten', 11 => 'Eleven', 12 => 'Twelve', 13 => 'Thirteen', 14 => 'Fourteen', 15 => 'Fifteen', 16 => 'Sixteen', 17 => 'Seventeen', 18 => 'Eighteen', 19 => 'Nineteen', 20 => 'Twenty', 30 => 'Thirty', 40 => 'Forty', 50 => 'Fifty', 60 => 'Sixty', 70 => 'Seventy', 80 => 'Eighty', 90 => 'Ninty' }
